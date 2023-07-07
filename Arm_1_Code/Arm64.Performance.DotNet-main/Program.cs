de51737eed07af6e88005388d31fcd8d2aa16fe6
ContentLab <114945818+contentlab-authors@users.noreply.github.com>
2023-07-07 10:17:49 -0400

    Add files via upload

---
+++ Arm_1_Code/Arm64.Performance.DotNet-main/Program.cs
@@ -0,0 +1,17 @@
+﻿using Arm64.Performance;
+
+Console.WriteLine($"Processor architecture: " +
+    $"{Environment.GetEnvironmentVariable("PROCESSOR_ARCHITECTURE")}");
+
+const int trialCount = 5;
+
+for ( int i = 0; i < trialCount; i++ )
+{
+    Console.WriteLine($"Trial no: {i + 1} / {trialCount}");
+
+    PerformanceHelper.MeasurePerformance(PerformanceTests.ListSorting, executionCount: 500, "List sorting");
+
+    PerformanceHelper.MeasurePerformance(PerformanceTests.SquareMatrixMultiplication, executionCount: 5, "Matrix multiplication");
+
+    PerformanceHelper.MeasurePerformance(PerformanceTests.StringOperations, executionCount: 500000, "String operations");
+}
